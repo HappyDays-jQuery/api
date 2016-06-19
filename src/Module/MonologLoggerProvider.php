15b11fe8140c22ede29c728f71643ac6f432a64a
HappyDays.jQuery <developer@jquery.tokyo>
2016-06-19 19:29:54 +0900

    monologプロバイダー追加

---
+++ src/Module/MonologLoggerProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace JqueryTokyo\Api\Module;
+
+use BEAR\AppMeta\AbstractAppMeta;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Ray\Di\ProviderInterface;
+
+class MonologLoggerProvider implements ProviderInterface
+{
+    /**
+     * @var AbstractAppMeta
+     */
+    private $appMeta;
+
+    public function __construct(AbstractAppMeta $appMeta)
+    {
+        $this->appMeta = $appMeta;
+    }
+
+    public function get()
+    {
+        $log = new Logger('api');
+        $log->pushHandler(
+            new StreamHandler($this->appMeta->logDir . '/api.log')
+        );
+
+        return $log;
+    }
+}
